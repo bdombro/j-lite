ee0aed711f4d50548713b2402f414b800f56d331
Brian Dombrowski <bdombro@gmail.com>
2025-05-19 10:33:58 +0200

    add back storybook

---
+++ .storybook/manager.ts
@@ -0,0 +1,12 @@
+import {addons} from '@storybook/manager-api'
+
+import Theme from './theme'
+
+addons.setConfig({
+  showPanel: false,
+  // showToolbar: false,
+  theme: Theme,
+  docs: {
+    theme: Theme,
+  },
+})
---
+++ .storybook/preview.tsx
@@ -0,0 +1,43 @@
+import './preview.css'
+
+import {Router, Switch} from '@slimr/react'
+import React from 'react'
+
+import '../src/foundation'
+import {ToastPack} from '../src/foundation'
+import Theme from './theme'
+
+export const decorators = [
+  (Story: React.FC<any>) => (
+    <Switch
+      router={
+        new Router({
+          notFound: {
+            component: Story,
+            exact: false,
+            path: '/',
+          },
+        })
+      }
+    />
+  ),
+  (Story: React.FC<any>) => (
+    <>
+      <ToastPack />
+      <Story />
+    </>
+  ),
+]
+
+export const parameters = {
+  actions: {argTypesRegex: '^on[A-Z].*'},
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  docs: {
+    theme: Theme,
+  },
+}
